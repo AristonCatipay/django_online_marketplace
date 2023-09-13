b9b34c4be79e767142a7c6ff93d02df819b19378
Ariston <aristoncatipay123@gmail.com>
2023-09-13 18:54:32 +0800

    Added the details of the item and the related items in detail.html

--- item/views.py
+++ item/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from . models import Item
 
 def detail(request, primary_key):
+    # Using the primary key we can get the specific item we want to display.
     item = get_object_or_404(Item, id = primary_key)
 
+    # We can also get the related items or items that are in the same category.
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(id=primary_key)[0:3]
+
     return render(request, 'item/detail.html', {
-        'item': item
+        'item': item,
+        'related_items' : related_items
     })
