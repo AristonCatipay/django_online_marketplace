edaa9dab734060258a7ca298e97ed415a2677e2f
Ariston <aristoncatipay123@gmail.com>
2023-09-13 14:51:10 +0800

    Created the Category and Item model and register them in the admin panel.

--- item/models.py
+++ item/models.py
@@ -1,5 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        # Setting the right plural name in the admin site.
+        verbose_name_plural = 'Categories'
+        # Setting the name field in alphabetical order.
+        ordering = ('name',)
+
+    # Showing the actual name of the field.
+    def __str__(self):
+        return self.name
     
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Showing the actual name of the field.
+    def __str__(self):
+        return self.name
+
+
