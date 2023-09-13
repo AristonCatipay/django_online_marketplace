72874dd215535a1991ffe3842dbb3ca20176b68f
Ariston <aristoncatipay123@gmail.com>
2023-09-13 17:55:39 +0800

    Fixed the unnecessary duplication of /contacts when opening the contacts page.

--- django_online_marketplace/urls.py
+++ django_online_marketplace/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.views import index
 # Imports for showing images.
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', index, name="index"),
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
     path('contact/', include('core.urls')),
     path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
