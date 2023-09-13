53a7596c90f81d0a03b5a7b16dc0f57ad1e00194
Ariston <aristoncatipay123@gmail.com>
2023-09-13 15:25:10 +0800

    Retrieve items from database and display them in index.html using cards.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from item.models import Category, Item
+
+
 
 def index(request):
-    return render(request, 'core/index.html')
+    # Retrieving only 6 items that is marked as unsold.
+    items = Item.objects.filter(is_sold = False)[0:6]
+    # Retrieving all the categories.
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
