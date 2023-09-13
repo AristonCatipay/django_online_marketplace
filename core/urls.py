6b945f8b1d2a9ccb7ffe938535258ab862fbe549
Ariston <aristoncatipay123@gmail.com>
2023-09-13 19:03:11 +0800

    Fix the redundancy in the main urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.contact, name='contact')
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact')
 ]
--- django_online_marketplace/urls.py
+++ django_online_marketplace/urls.py
@@ -22,8 +22,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('admin/', admin.site.urls),
-    path('contact/', include('core.urls')),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
