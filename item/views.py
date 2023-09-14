c17185a8c2bf0dffd2814ca9fc7ed3808af91a3d
Ariston <aristoncatipay123@gmail.com>
2023-09-14 20:56:28 +0800

    Created the new items page and it's functionality.

--- item/views.py
+++ item/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from . models import Item
+from . forms import NewItemForm
 
 def detail(request, primary_key):
     # Using the primary key we can get the specific item we want to display.
@@ -11,4 +13,24 @@ def detail(request, primary_key):
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items' : related_items
+    })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            # Before we commit we need to add the user that made the request to the query.
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', primary_key = item.id)
+    else: 
+        form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New Item'
     })
