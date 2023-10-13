9ab0e9876a23fbe5b01d3ba67d4df8dd7eacb832
Ariston <aristoncatipay123@gmail.com>
2023-10-13 23:37:55 +0800

    Created the index page of user_profile.

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
+from . models import Profile
+
 def index(request):
+    user = User.objects.get(username=request.user.username)
+    profile = Profile.objects.get(user=user)
+
     return render(request, 'profile/index.html', {
         'title': 'Profile',
+        'profile': profile,
+        'user': user,
     })
