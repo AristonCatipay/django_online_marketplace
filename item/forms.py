f2df1e4dc783a576714265fd10af6ea99275c5c0
Ariston <aristoncatipay123@gmail.com>
2023-10-14 21:46:10 +0800

    Recreated the design for the sell item form.

--- item/forms.py
+++ item/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from . models import Item
 
-INPUT_CLASSES = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
+INPUT_CLASSES = 'border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
 FOR_IMAGE = 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'
 
 class NewItemForm(forms.ModelForm):
