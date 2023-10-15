f560f4051c4f0051a066a8fdded45cb51c97d091
Ariston <aristoncatipay123@gmail.com>
2023-10-15 14:15:09 +0800

    Created the edit profile page and added it to the urls.

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'profile'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('edit/', views.edit, name='edit'),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -13,3 +13,13 @@ def index(request):
         'profile': profile,
         'user': user,
     })
+
+def edit(request):
+    user = User.objects.get(username=request.user.username)
+    profile = Profile.objects.get(user=user)
+
+    return render(request, 'profile/edit.html', {
+        'title': 'Edit Profile',
+        'profile': profile,
+        'user': user,
+    })
