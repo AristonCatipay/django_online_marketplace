0e1562d9884cf68d4df2c02255c1e1b8fafe60ce
Ariston <aristoncatipay123@gmail.com>
2023-11-30 18:33:40 +0800

    Create test for item detail url.

--- item/tests/test_urls.py
+++ item/tests/test_urls.py
@@ -1,8 +1,46 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse, resolve
-from item.views import items
+from django.contrib.auth.models import User
+from item.views import items, detail
+from item.models import Category, Item
 
-class ItemUrlTestCase(SimpleTestCase):
+class ItemUrlTestCase(TestCase):
+    def setUp(self):
+        self.test_user = self.create_test_user()
+        self.test_category = self.create_test_category()
+        self.test_item = self.create_test_item()
+
+    def tearDown(self):
+        self.test_item.delete()
+        self.test_category.delete()
+        self.test_user.delete()
+
     def test_index_url(self):
         url = reverse('item:items')
         self.assertEquals(resolve(url).func, items)
+    
+    def create_test_user(self):
+        return User.objects.create_user(
+            first_name = 'firstname test',
+            last_name = 'lastname test',
+            username = 'testuser',
+            email = 'emailtest',
+            password = '12345'
+        )
+
+    def create_test_category(self):
+        return Category.objects.create(name = 'test category')
+    
+    def create_test_item(self):
+        return Item.objects.create(
+            category = self.test_category,
+            created_by = self.test_user,
+            name = 'test item',
+            description = 'test description',
+            price = 100,
+            image = 'default_profile_image.jpg'
+        )
+
+    def test_detail_url(self):
+        url = reverse('item:detail', args = [self.test_item.pk])
+        self.assertEquals(resolve(url).func, detail)
