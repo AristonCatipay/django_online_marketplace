403498c8759fd53f7f5e0d27bf65572684eb8392
Ariston <aristoncatipay123@gmail.com>
2023-12-06 22:05:10 +0800

    Create test data for testing item model.

--- item/tests/test_models.py
+++ item/tests/test_models.py
@@ -8,3 +8,14 @@ class CategoryModelTest(TestCase):
 
     def tearDown(self):
         self.category.delete()
+
+class ItemModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.category = Category.objects.create(name='Test Category')
+        self.item = Item.objects.create(category=self.category, created_by=self.user, name='Test Item', price=50)
+
+    def tearDown(self):
+        self.item.delete()
+        self.category.delete()
+        self.user.delete()
