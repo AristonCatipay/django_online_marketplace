b381e1a4abfbde6998df42fee650d171f917a245
Ariston <aristoncatipay123@gmail.com>
2023-12-21 16:20:47 +0800

    Install django rest framework and set up the necessary files.

--- django_online_marketplace/settings.py
+++ django_online_marketplace/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'tailwind',
     'theme',
     'django_browser_reload',
