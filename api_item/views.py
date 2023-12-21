08595fd7c6ed5eb422785270a7cf219c6b9188a8
Ariston <aristoncatipay123@gmail.com>
2023-12-21 16:29:46 +0800

    Create an API endpoint that will retrieve all of the items for sale.

--- api_item/views.py
+++ api_item/views.py
@@ -2,8 +2,25 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Q
+from .serializers import ItemSerializer
+from item.models import Item, Category
+
 
 @api_view(['GET'])
-def getData(request):
-    test_data = {"name": "Testing"}
-    return Response(test_data)
+def read_items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+    
+    if category_id:
+        items = items.filter(category_id = category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
+
+    
