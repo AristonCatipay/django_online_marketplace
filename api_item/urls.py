6dc49c97ae5c404fcf40ea968357b9de888e89f8
Ariston <aristoncatipay123@gmail.com>
2023-12-21 21:08:09 +0800

    Create an API endpoint to retrive the details of a specific item.

--- api_item/urls.py
+++ api_item/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.read_items),
+    path('detail/<int:item_primary_key>/', views.read_item_detail)
 ]
--- api_item/views.py
+++ api_item/views.py
@@ -3,11 +3,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from .serializers import ItemSerializer
 from item.models import Item, Category
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def read_items(request):
     query = request.GET.get('query', '')
     category_id = request.GET.get('category', 0)
@@ -22,5 +24,30 @@ def read_items(request):
 
     serializer = ItemSerializer(items, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def read_item_detail(request, item_primary_key):
+    item = get_object_or_404(Item, id=item_primary_key)
+    seller_profile = item.created_by.profile
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(id=item_primary_key)[0:3]
+    serializer = ItemSerializer(item)
+    serialized_item = serializer.data
+
+    serialized_item['seller'] = {
+        'first_name': seller_profile.user.first_name,
+        'last_name': seller_profile.user.last_name,
+        'username': seller_profile.user.username,
+        'email': seller_profile.user.email,
+        'location': seller_profile.location,
+    }
+    serialized_related_items = ItemSerializer(related_items, many=True).data
+
+    response_data = {
+        'item': serialized_item,
+        'related_items': serialized_related_items,
+    }
+
+    return Response(response_data)
 
     
