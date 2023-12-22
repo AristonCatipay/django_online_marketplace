446b5b4505ab87cd3f3a27fbc896f2fa028e5d5a
Ariston <aristoncatipay123@gmail.com>
2023-12-22 20:12:41 +0800

    Create an API endpoint for retrieving current user profile data.

--- api_user_profile/urls.py
+++ api_user_profile/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('read/', views.read_current_user_profile),
+]
--- api_user_profile/views.py
+++ api_user_profile/views.py
@@ -2,3 +2,13 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+from user_profile.models import Profile
+from .serializers import ProfileSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def read_current_user_profile(request):
+    current_user_profile = get_object_or_404(Profile, user=request.user)
+    profile_serializer = ProfileSerializer(current_user_profile, many=False)
+    return Response(profile_serializer.data)
