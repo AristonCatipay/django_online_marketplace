469675a4b18645256665515ade5ec252eb9d2ce1
Ariston <aristoncatipay123@gmail.com>
2023-12-22 22:58:32 +0800

    Create the Conversation Message Serializer.

--- api_conversation/serializers.py
+++ api_conversation/serializers.py
@@ -5,3 +5,8 @@ class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = '__all__'
+
+class ConversationMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConversationMessage
+        fields = '__all__'
