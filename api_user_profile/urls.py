643667ac9e2b28d09f2715b5c055a96e863a64a2
Ariston <aristoncatipay123@gmail.com>
2023-12-22 20:56:12 +0800

    Create an API endpoint for updating current user profile data.

--- api_user_profile/urls.py
+++ api_user_profile/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('read/', views.read_current_user_profile),
+    path('update/', views.update_current_user_profile),
 ]
--- api_user_profile/views.py
+++ api_user_profile/views.py
@@ -12,3 +12,13 @@ def read_current_user_profile(request):
     current_user_profile = get_object_or_404(Profile, user=request.user)
     profile_serializer = ProfileSerializer(current_user_profile, many=False)
     return Response(profile_serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_current_user_profile(request):
+    current_user_profile = get_object_or_404(Profile, user=request.user)
+    profile_serializer = ProfileSerializer(current_user_profile, data=request.data)
+    if profile_serializer.is_valid():
+        profile_serializer.save()
+        return Response(profile_serializer.data)
+    return Response(profile_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
