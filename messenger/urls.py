9f096bd5b05fe3e16308217f959afe19ff1794d6
Ariston <aristoncatipay123@gmail.com>
2024-02-16 21:41:14 +0800

    Create the view messages functionality.

--- messenger/urls.py
+++ messenger/urls.py
@@ -5,4 +5,5 @@ app_name = 'messenger'
 
 urlpatterns = [
     path('inbox/', views.inbox, name='inbox'),
+    path('messages/<int:metadata_primary_key>/', views.messages, name='messages'),
 ]
--- messenger/views.py
+++ messenger/views.py
@@ -14,3 +14,29 @@ def inbox(request):
         'title': 'Messenger',
         'metadata': metadata,
     })
+
+@login_required()
+def messages(request, metadata_primary_key):
+    metadata = Metadata.objects.filter(members__in=[request.user.id]).get(pk=metadata_primary_key)
+    reciever = metadata.members.exclude(id=request.user.id).first()
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            # Save the message
+            message = form.save(commit=False)
+            message.metadata = metadata
+            message.created_by = request.user
+            message.save()
+
+            metadata.save()
+            return redirect('messenger:messages', metadata_primary_key=metadata_primary_key)
+    else:
+        form = MessageForm()
+
+    return render(request, 'messenger/messages.html', {
+        'title': 'Send Message',
+        'metadata': metadata,
+        'reciever': reciever,
+        'form': form,
+    })
