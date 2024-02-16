747392879e86e86d1cf0998213f9d48538ac3b93
Ariston <aristoncatipay123@gmail.com>
2024-02-16 21:36:16 +0800

    Delete the conversation and api conversation apps.

--- api_conversation/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('read/', views.read_inbox),
-    path('create/<int:item_primary_key>/', views.create_new_conversation),
-    path('messages/<int:conversation_primary_key>/', views.read_conversation_messages),
-    path('messages/create/<int:conversation_primary_key>/', views.create_message),
-]
