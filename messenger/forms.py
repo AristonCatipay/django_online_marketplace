fd32d8e84eef775ab586826ab33aa3bd28f07959
Ariston <aristoncatipay123@gmail.com>
2024-02-16 22:49:35 +0800

    Update the design of message page.

--- messenger/forms.py
+++ messenger/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Message
 
-INPUT_CLASSES = 'block w-full p-2 text-sm text-gray-900 border border-violet-300 rounded-lg bg-violet-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-violet-700 dark:border-violet-600 dark:placeholder-gray-200 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+INPUT_CLASSES = 'block w-full p-2 text-sm text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
