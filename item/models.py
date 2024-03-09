b52d00968b9338667507128c7eef1b0ed591229a
Ariston <aristoncatipay123@gmail.com>
2024-03-09 11:45:39 +0800

    Create the default categories for the category model when you run the first migration.

--- item/models.py
+++ item/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -13,6 +15,40 @@ class Category(models.Model):
     # Showing the actual name of the field.
     def __str__(self):
         return self.name
+    
+PREDEFINED_CATEGORIES = [
+    "Women's Clothing and Shoes",
+    "Men's Clothing and Shoes",
+    "Toys and Games",
+    "Vehicles",
+    "Furniture",
+    "Electronics",
+    "Properties for Rent",
+    "Properties for Sale",
+    "Antiques and collectibles",
+    "Appliances",
+    "Arts and Crafts",
+    "Baby",
+    "Books, Films and Music",
+    "Car parts",
+    "DIY and Tools",
+    "Health and Beauty",
+    "Home goods and decor",
+    "Jewellery and watches",
+    "Luggage and bags",
+    "Musical Instruments",
+    "Pet supplies",
+    "Vehicles",
+    "Not Specified"
+]
+
+@receiver(post_migrate)
+def check_categories(sender, **kwargs):
+    # Check if Category table is empty
+    if Category.objects.count() == 0:
+        # If empty, populate with predefined categories
+        for category_name in PREDEFINED_CATEGORIES:
+            Category.objects.create(name=category_name)
     
 class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
